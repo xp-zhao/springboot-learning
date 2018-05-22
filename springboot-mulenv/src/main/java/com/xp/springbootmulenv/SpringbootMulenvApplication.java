4390a16579183a97a81c08e585b0d3c92661ddb8
xp-zhao <13688396271@163.com>
2018-05-22 17:44:38 +0800

    springboot 项目多环境配置学习

---
+++ springboot-mulenv/src/main/java/com/xp/springbootmulenv/SpringbootMulenvApplication.java
@@ -0,0 +1,12 @@
+package com.xp.springbootmulenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMulenvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMulenvApplication.class, args);
+	}
+}
