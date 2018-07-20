107b2a516c4eacc1aeec136151914803f3c7b355
xp-zhao <13688396271@163.com>
2018-07-20 10:23:42 +0800

    'redis'

--- springboot-redis/src/main/java/com/example/springbootredis/SpringbootRedisApplication.java
+++ springboot-redis/src/main/java/com/example/springbootredis/SpringbootRedisApplication.java
@@ -2,6 +2,11 @@ package com.example.springbootredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 public class SpringbootRedisApplication {
